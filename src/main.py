97a0990d38f9eef1812771df02f355c0ad3001e8
hindawi <ahmedhindawi2001@gmail.com>
2023-08-14 06:52:47 +0300

    init leescrap project

---
+++ src/main.py
@@ -0,0 +1,6 @@
+from leetscrape.GetQuestionsList import GetQuestionsList
+from leetscrape.GetQuestionInfo import GetQuestionInfo
+from leetscrape.utils import combine_list_and_info, get_all_questions_body
+
+
+ls = GetQuestionsList()
